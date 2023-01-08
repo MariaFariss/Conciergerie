22c526b5eff12b67859605ef7c27de8f825a6e26
Axel Villeret <91218852+AxelVilleret@users.noreply.github.com>
2023-01-08 18:10:02 +0100

    maj db

---
+++ db.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 08 jan. 2023 à 17:08
+-- Version du serveur : 5.7.36
+-- Version de PHP : 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `conciergerie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_expedition`
+--
+
+DROP TABLE IF EXISTS `commande_expedition`;
+CREATE TABLE IF NOT EXISTS `commande_expedition` (
+  `id_commande` int(11) NOT NULL,
+  `id_expedition` int(11) NOT NULL,
+  PRIMARY KEY (`id_commande`,`id_expedition`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

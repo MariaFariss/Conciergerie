b80ec52466039b0f4140304c305689308bdf63d0
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-01-09 23:15:40 +0100

    commit

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
         if ($_GET['action'] === 'gestion_clients') {
             (new GestionClients())->execute();
-        } else {
+        }
+        else if ($_GET['action'] === 'ajout_client') {
+            (new GestionClients())->execute();
+        }
+        else {
             throw new Exception("La page que vous recherchez n'existe pas.");
         }
+        
     } else {
         (new Accueil())->execute();
     }
--- templates/gestion_clients.php
+++ templates/gestion_clients.php
@@ -1,13 +1,56 @@
 <?php $title = "Gestion des clients"; ?>
 
 <?php ob_start(); ?>
-<?php require('background.php') ?>
+<!-- <?php require('background.php') ?> -->
 <h1 class="title">GESTION DES CLIENTS</h1>
 <iconify-icon icon="ic:round-person-add" width="40" class="icone1"></iconify-icon>
 <iconify-icon icon="material-symbols:home" width="40" class="icone2"></iconify-icon>
-<?php
+<table border=1 frame=void rules=rows>
+    <thead>
+        <th>N°</th>
+        <th>Nom</th>
+        <th>Tel</th>
+        <th>Email</th>
+        <th></th>
+        <th></th>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($clients as $client) {
+        ?>
+            <tr>
+                <td>
+                    <?= $client->id_client ?>
+                </td>
+                <td>
+                    <?= $client->nom ?>
+                </td>
+                <td>
+                    <?= $client->telephone ?>
+                </td>
+                <td>
+                    <?= $client->email ?>
+                </td>
+                <td>
+                    <a href="#">
+                        <iconify-icon icon="ic:baseline-remove-red-eye" width="30"></iconify-icon>
+                    </a>
+                </td>
+                <td>
+                    <a href="#">
+                        <iconify-icon icon="material-symbols:edit" width="30"></iconify-icon>
+                    </a>
+                </td>
 
-?>
+
+            </tr>
+        <?php
+        }
+        ?>
+
+    </tbody>
+</table>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('layout.php') ?>

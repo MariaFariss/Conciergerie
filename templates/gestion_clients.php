fac1b2724985fc7511a4ec0925501ab0bb73d36f
Axel Villeret <91218852+AxelVilleret@users.noreply.github.com>
2023-01-11 09:25:10 +0100

    styles gestion

--- templates/gestion_clients.php
+++ templates/gestion_clients.php
@@ -1,55 +1,60 @@
 <?php $title = "Gestion des clients"; ?>
 
 <?php ob_start(); ?>
-<!-- <?php require('background.php') ?> -->
+<?php require('background.php') ?>
 <h1 class="title">GESTION DES CLIENTS</h1>
 <iconify-icon icon="ic:round-person-add" width="40" class="icone1"></iconify-icon>
 <iconify-icon icon="material-symbols:home" width="40" class="icone2"></iconify-icon>
-<table border=1 frame=void rules=rows>
-    <thead>
-        <th>N°</th>
-        <th>Nom</th>
-        <th>Tel</th>
-        <th>Email</th>
-        <th></th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php
-        foreach ($clients as $client) {
-        ?>
-            <tr>
-                <td>
-                    <?= $client->id_client ?>
-                </td>
-                <td>
-                    <?= $client->nom ?>
-                </td>
-                <td>
-                    <?= $client->telephone ?>
-                </td>
-                <td>
-                    <?= $client->email ?>
-                </td>
-                <td>
-                    <a href="#">
-                        <iconify-icon icon="ic:baseline-remove-red-eye" width="30"></iconify-icon>
-                    </a>
-                </td>
-                <td>
-                    <a href="#">
-                        <iconify-icon icon="material-symbols:edit" width="30"></iconify-icon>
-                    </a>
-                </td>
+<div class="ligne">
+    <table border=1 frame=void rules=rows>
+        <thead>
+            <th>N°</th>
+            <th>Nom</th>
+            <th>Tel</th>
+            <th>Email</th>
+            <th></th>
+            <th></th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($clients as $client) {
+            ?>
+
+                <tr>
+
+                    <td>
+                        <?= $client->id_client ?>
+                    </td>
+                    <td>
+                        <?= $client->nom ?>
+                    </td>
+                    <td>
+                        <?= $client->telephone ?>
+                    </td>
+                    <td>
+                        <?= $client->email ?>
+                    </td>
+                    <td>
+                        <a href="#">
+                            <iconify-icon icon="ic:baseline-remove-red-eye" width="30"></iconify-icon>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#">
+                            <iconify-icon icon="material-symbols:edit" width="30"></iconify-icon>
+                        </a>
+                    </td>
+
+                </tr>
 
 
-            </tr>
-        <?php
-        }
-        ?>
+            <?php
+            }
+            ?>
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 

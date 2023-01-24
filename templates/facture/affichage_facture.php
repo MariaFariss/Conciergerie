55af1c117855d3950cd26cc8e611118a0d2f2326
Axel Villeret <91218852+AxelVilleret@users.noreply.github.com>
2023-01-24 20:42:25 +0100

    commit

--- templates/facture/affichage_facture.php
+++ templates/facture/affichage_facture.php
@@ -1,21 +1,28 @@
 <?php $title = "Gestion des clients"; ?>
 
 <?php ob_start(); ?>
-<!-- <?php require('templates/autres/background.php') ?> -->
+<?php require('templates/autres/background.php') ?>
 <h1 class="title">GESTION DES COMMANDES</h1>
 <a href="index.php?action=export_csv"><iconify-icon icon="material-symbols:export-notes" width="40" class="icone1"></iconify-icon></a>
 <a href="index.php"><iconify-icon icon="material-symbols:home" width="40" class="icone2"></iconify-icon></a>
 
-<div>
-    <h2>Facture</h2>
-    <p>Numéro de facture : <?= $facture->id_facture ?></p>
-    <p>Date de la facture : <?= $facture->date_facture ?></p>
-    <p>Client : <?= $facture->nom_client ?></p>
-    <p>Adresse : <?= $facture->adresse_client ?></p>
-    <p>Numéro de téléphone : <?= $facture->telephone_client ?></p>
-    <p>Adresse email : <?= $facture->email_client ?></p>
+<div class="ligne2">
+    <p>No. Commande : <?= $commande->id_commande ?></p>
+    <p>Date de commande : <?php $date = new DateTime($commande->date_commande);
+                            echo ($date->format("Y-m-d")) ?></p>
+    <p>No. Facture : <?= $facture->id_facture ?></p>
+    <p>Date Facture : <?php $date = new DateTime($facture->date_creation);
+                        echo ($date->format("Y-m-d")) ?></p>
 </div>
-<div class="ligne">
+
+<div class="ligne2" style="float: right;">
+    <p>No. Client : <?= $client->id_client ?></p>
+    <p>Nom Client : <?= $client->nom ?></p>
+    <p>Adresse : <?= $client->adresse ?></p>
+    <p>Téléphone : <?= $client->telephone ?></p>
+</div>
+
+<div class="ligne2">
     <table border=1 frame=void rules=rows>
         <thead>
             <th>N°</th>
@@ -42,7 +49,7 @@
                         <?= $article->prix_magasin ?> $
                     </td>
                     <td>
-                        <?= $article->prix_magasin * $quantites[$article->id_article] ?> 
+                        <?= $article->prix_magasin * $quantites[$article->id_article] ?>
                     </td>
                 </tr>
 
@@ -52,6 +59,14 @@
 
         </tbody>
     </table>
+</div>
+
+<div class="ligne2" style="float: right;">
+    <p>Montant Commande : <?= $commande->total ?> $</p>
+    <p>Frais Livraison : <?= $commande->frais_livraison ?> $</p>
+    <p>Promotion/Remise : 0 $</p>
+    <p>Dépôt : <?= $commande->frais_depot ?> $</p>
+    <p>Montant de la Facture : <?= $facture->montant ?> $</p>
 </div>
 
 <?php $content = ob_get_clean(); ?>
